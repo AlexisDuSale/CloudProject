fd7cf5469fbc19deeabf2039f18a682e0fc1db25
kberrebi <berrebi.kylian@gmail.com>
2017-10-31 11:50:12 +0100

    adding the new files

---
+++ src/KafkaConsumerTest.java
@@ -0,0 +1,41 @@
+
+//package kakfa;
+
+import java.util.Properties;
+import java.util.Arrays;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.errors.WakeupException;
+
+
+public class KafkaConsumerTest {
+
+	private static final String topicName = "conso-maisons";
+
+	public static void main(String[] args) {
+	
+		
+	
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("group.id", "groupName");
+		props.put("enable.auto.commit", "true");
+		props.put("auto.commit.interval.ms", "1000");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+		consumer.subscribe(Arrays.asList(topicName));
+		try{
+			while (true) {
+				ConsumerRecords<String, String> records = consumer.poll(100);
+				for (ConsumerRecord<String, String> record : records)
+					System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+			}
+		} catch (WakeupException e) {
+			//ignore for shutdown
+		} finally {
+			consumer.close();	
+		}
+	}
+
+}
